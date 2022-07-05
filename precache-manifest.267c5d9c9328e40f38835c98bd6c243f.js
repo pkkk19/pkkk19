b8d983d5752b6158063fb6f363b1b57b89faee25
developer <prabesh065@gmail.com>
2022-07-05 15:56:48 +0545

    Updates

---
+++ precache-manifest.267c5d9c9328e40f38835c98bd6c243f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a98062c92f8032caf76",
+    "url": "/pkkk19/static/css/main.692cfba9.chunk.css"
+  },
+  {
+    "revision": "5a98062c92f8032caf76",
+    "url": "/pkkk19/static/js/main.5a98062c.chunk.js"
+  },
+  {
+    "revision": "6a7bfa4df9242b40edff",
+    "url": "/pkkk19/static/js/1.6a7bfa4d.chunk.js"
+  },
+  {
+    "revision": "49ef6b28dfe44187afea",
+    "url": "/pkkk19/static/css/2.05c4f75d.chunk.css"
+  },
+  {
+    "revision": "49ef6b28dfe44187afea",
+    "url": "/pkkk19/static/js/2.49ef6b28.chunk.js"
+  },
+  {
+    "revision": "ae3dc6c51cf8317a300a",
+    "url": "/pkkk19/static/js/runtime~main.ae3dc6c5.js"
+  },
+  {
+    "revision": "e8a0d07265de7977ba192ce96e88a096",
+    "url": "/pkkk19/static/media/profile-img.e8a0d072.jpg"
+  },
+  {
+    "revision": "fd14f7e0fa2b7857451c825b2a8652f5",
+    "url": "/pkkk19/static/media/hero-bg.fd14f7e0.jpg"
+  },
+  {
+    "revision": "3a9baefd4c27f48fa80f44d912edc4dd",
+    "url": "/pkkk19/index.html"
+  }
+];
